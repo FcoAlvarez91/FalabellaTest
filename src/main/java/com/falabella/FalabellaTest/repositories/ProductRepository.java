101c9c0f7a64427b437da0d1dd47d3728903ddd7
TheMech <fcoalvarez91@gmail.com>
2022-02-24 18:11:49 -0300

    CRUD Added, Update WIP

--- src/main/java/com/falabella/FalabellaTest/repositories/ProductRepository.java
+++ src/main/java/com/falabella/FalabellaTest/repositories/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.falabella.FalabellaTest.repositories;
 
+import java.util.ArrayList;
+
 import com.falabella.FalabellaTest.models.ProductModel;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends CrudRepository<ProductModel, Long> {
-    
+    public abstract ArrayList<ProductModel> findBySku(String sku);
 }
--- src/main/java/com/falabella/FalabellaTest/services/ProductService.java
+++ src/main/java/com/falabella/FalabellaTest/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.falabella.FalabellaTest.services;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import com.falabella.FalabellaTest.models.ProductModel;
 import com.falabella.FalabellaTest.repositories.ProductRepository;
@@ -20,4 +21,22 @@ public class ProductService {
     public ProductModel saveProduct(ProductModel product){
         return productRepository.save(product);
     }
+
+    public Optional<ProductModel> getById(Long id){
+        return productRepository.findById(id);
+    }
+
+    public ArrayList<ProductModel> getBySku(String sku){
+        return productRepository.findBySku(sku);
+    }
+
+    public boolean deleteProduct(Long id){
+        try {
+            productRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
